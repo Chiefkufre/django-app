7f49133696631a728304ffa3eaa4bbcbaf6578fd
chiefkufre <kufhub@gmail.com>
2022-08-01 20:20:04 -0700

    chore: add more views to controller

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.login, name='login')
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<str:question_id>/results/', views.results, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,28 @@
+from multiprocessing import context
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Question
+from django.template import loader
 
 
 # Create your views here.
 
 def index(request):
+    latest_question_list = Question.objects.order_by('-pubDate')[:5]
     
-    return HttpResponse("Hello, welcome to my first django application")
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
-def login(request):
-    return JsonResponse({
-        "msg": "return back jsonify response"
-    })
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s. " % question_id)
+
+def results(request, question_id):
+    response = "you are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s. " % question_id)
